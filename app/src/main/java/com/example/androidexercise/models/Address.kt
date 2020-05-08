ff941c1883dc0701c25fc880908838ddb7131c37
Harshit Singh <harshit@vinsol.com>
2020-05-08 16:34:05 +0530

    New changes added

--- app/src/main/java/com/example/androidexercise/models/Address.kt
+++ app/src/main/java/com/example/androidexercise/models/Address.kt
@@ -1,12 +1,10 @@
 package com.example.androidexercise.models
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
 /*
 Model data class Address to give a format to the address
  */
-@Parcelize
 data class Address(
     val id: Int,
     val firstname: String,
@@ -19,4 +17,47 @@ data class Address(
     val company: String?,
     val state_id: Int?,
     val country_id: Int?
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(firstname)
+        parcel.writeString(address1)
+        parcel.writeString(address2)
+        parcel.writeString(city)
+        parcel.writeString(zipcode)
+        parcel.writeString(state_name)
+        parcel.writeString(alternate_phone)
+        parcel.writeString(company)
+        parcel.writeValue(state_id)
+        parcel.writeValue(country_id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Address> {
+        override fun createFromParcel(parcel: Parcel): Address {
+            return Address(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Address?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
